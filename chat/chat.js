83a88b7ad5e10d635b14f6cf127f1aae98dc496f
Andre Kless <andre.kless@inf.h-brs.de>
2016-05-18 14:28:40 +0200

    jsonp test

--- chat/chat.js
+++ chat/chat.js
@@ -8,7 +8,7 @@ ccm.component( {
 
   config: {
 
-    html:  [ ccm.store, { local: 'http://akless.github.io/ccm-developer/chat/templates.js' } ],
+    html:  [ ccm.store, 'http://akless.github.io/ccm-developer/chat/templatesp.json' ],
     key:   'test',
     store: [ ccm.store, { url: 'ws://ccm2.inf.h-brs.de/index.js', store: 'chat' } ],
     style: [ ccm.load, 'http://akless.github.io/ccm-developer/chat/style.css' ],
